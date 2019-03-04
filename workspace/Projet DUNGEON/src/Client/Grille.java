9f0c783e097c1f6f1a2fbeb347765b106ba4867b
DEMBELE Boubacar <boubacardembele41@gmail.com>
2019-03-04 09:01:22 +0100

    Dev creer personnage

--- workspace/Projet DUNGEON/src/Client/Grille.java
+++ workspace/Projet DUNGEON/src/Client/Grille.java
@@ -2,4 +2,47 @@ package Client;
 
 public class Grille {
 
+	// Attributs
+	private int nbLig;
+	private int nbCol;
+	private char[][] grille;
+
+	// Constructeur
+	public Grille(int nbLig, int nbCol) {
+		super();
+		this.nbLig = nbLig;
+		this.nbCol = nbCol;
+		this.grille = new char[nbLig][nbCol];
+
+		for (int i = 0; i < this.nbLig; i++) {
+			for (int j = 0; j < this.nbCol; j++) {
+				grille[i][j] = 'x';
+			}
+		}
+	}
+
+	// Accesseurs
+
+	// Méthodes
+	public void afficher() {
+		System.out.println();
+		for (int i = 0; i < this.nbLig; i++) {
+			for (int j = 0; j < this.nbCol; j++) {
+				System.out.print(" | " + grille[i][j]);
+			}
+			System.out.print(" | \n");
+		}
+		System.out.println();
+	}
+
+	public void initialiser(char initial) {
+		grille[4][1] = initial;
+		this.afficher();
+	}
+
+	public static void main(String[] args) {
+		Grille g = new Grille(5, 5);
+		g.afficher();
+	}
+
 }
--- workspace/Projet DUNGEON/src/Client/Utilisateur.java
+++ workspace/Projet DUNGEON/src/Client/Utilisateur.java
@@ -11,28 +11,66 @@ public class Utilisateur {
 	public static void main(String[] args) throws Exception {
 
 		Scanner sc = new Scanner(System.in);
+		Scanner str = new Scanner(System.in);
+		Boolean Jouer = true;
+
 		Server_L proxy = (Server_L) Naming.lookup("rmi://localhost:1099/ServeurLabyrinthe");
+
 		System.out.println("Bienvenu sur le server Labyrinthe");
-		System.out.println("Pour jouer, vous devez créer un personnage.");
-		System.out.println("Saisir le chiffre correspondant à votre souhait:");
-		System.out.println("1: Créer un personnage");
-		System.out.println("0: Quitter le jeu");
 
-		int i = sc.nextInt();
-		switch (i) {
-		case 1:
-			System.out.println("Donner le nom de votre personnage:");
-			String nom = sc.nextLine();
-			if (proxy.creerPersonnage(nom)) {
-				System.out.println("Le personnage "+nom+" a été créé !");
+		while (true) {
+			System.out.println("Pour jouer, vous devez créer un personnage.");
+			System.out.println("Saisir le chiffre correspondant à votre souhait:");
+			System.out.println("1: Créer un personnage");
+
+			int i = sc.nextInt();
+
+			switch (i) {
+			case 1:
+				Jouer= true;
+				Grille jeu = new Grille(5, 5);
+				System.out.println("Donner le nom de votre personnage:");
+				String nom = str.nextLine();
+				char initial = nom.charAt(0);
+				if (proxy.creerPersonnage(nom)) {
+					System.out.println("Le personnage " + nom + " a été créé !");
+					System.out.println("La position actuelle de " + nom
+							+ " dans le labyrinthe est representé par la lettre " + initial);
+					jeu.initialiser(initial);
+				}
+				while (Jouer) {
+					System.out.println("Saisir le chiffre correspondant à votre souhait:");
+					System.out.println("1: Se deplacer");
+					System.out.println("0: Quitter le jeu");
+
+					i = sc.nextInt();
+					if (i == 1) {
+						char cmd;
+						do {
+							System.out.println("Quelle direction souhaitez vous (E,O,N,S):");
+							String cmd_s = str.nextLine();
+							cmd_s = cmd_s.toUpperCase();
+							cmd = cmd_s.charAt(0);
+							if (cmd == 'E' || cmd == 'S' || cmd == 'O' || cmd == 'N') {
+								proxy.deplacer(nom, cmd);
+								//
+							} else {
+								System.out.println("Votre commande n'a pas été comprise.");
+							}
+						} while ((cmd != 'E') && (cmd != 'S') && (cmd != 'O') && (cmd != 'N'));
+
+					} else {
+						Jouer = false;
+					}
+				}
+
+				break;
+
+			default:
+				break;
 			}
-			break;
-
-		default:
-			break;
 		}
 
-	
 	}
 
 }
